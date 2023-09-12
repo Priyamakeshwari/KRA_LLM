20fc95cd4f5259673ed7f9719bb715c9f8347d55
Kuldeep <71772185+neokd@users.noreply.github.com>
2023-09-13 00:31:56 +0530

    added weaviate db test

---
+++ backend/weaviate_db.py
@@ -0,0 +1,80 @@
+import weaviate
+import json
+import sys
+import uuid
+client = weaviate.Client("http://localhost:8080")
+
+# Delete any existing schema (optional)
+client.schema.delete_all()
+
+# Define the schema for the class with a vectorizer
+schema = {
+    "classes": [
+        {
+            "class": "Product",
+            "description": "Holds the information about same data",
+            "properties": [
+                {
+                    "name": "title",
+                    "description": "Title of the product",
+                    "dataType": ["text"],
+                },
+                {
+                    "name": "description",
+                    "description": "Description of the product",
+                    "dataType": ["text"],
+                },
+                {
+                    "name": "price",
+                    "description": "Price of the product",
+                    "dataType": ["number"],
+                }
+            ],
+            "vectorizer": "text2vec-huggingface",
+            "moduleConfig": {
+                "model": "sentence-transformers/all-MiniLM-L6-v2",
+            }
+        }
+    ],
+
+}
+
+# Create the schema in Weaviate
+client.schema.create(schema)
+
+# Load Products data
+with open('backend/data.json', 'r') as f:
+    product_data = json.load(f)
+
+vectorized_products = []
+
+for product_info in product_data:
+    title = product_info['title']
+    description = product_info['description']
+    price = product_info['price']
+
+    vectorized_products.append({
+        'product_id': str(uuid.uuid4()),
+        "title": title,
+        "description": description,
+        "price": price
+    })
+
+# Create the products in Weaviate
+for product in vectorized_products:
+    print(product)
+    client.data_object.create(product, "Product")
+
+
+for input in sys.stdin:
+    response = (
+        client.query
+        .get("Product", ["title", "description", "price"])
+        .with_near_text({
+            "concepts": [input]
+        }).with_limit(1)
+        .with_additional(["distance"])
+        .do()
+    )
+    
+    print(json.dumps(response, indent=2))
